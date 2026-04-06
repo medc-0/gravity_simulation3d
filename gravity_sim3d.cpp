5434d749e5dad77ab5349f4f8f45eea0938075f4
omedcodes <omedsafi21@gmail.com>
2026-04-06 12:09:39 +0200

    Implement Object class

--- gravity_sim3d.cpp
+++ gravity_sim3d.cpp
@@ -46,6 +46,105 @@ void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
 void scroll_callback(GLFWwindow* window, double xOffset, double yOffset);
 
 void mouse_callback(GLFWwindow* window, double xPos, double yPos);
-glm::vec sphericalToCartesian(float r, float theta, float phi);  // 0 theta p phi r=how far from center
+glm::vec3 sphericalToCartesian(float r, float theta, float phi);  // 0 theta p phi r=how far from center
 void DrawGrid(GLuint shaderProgram, GLuint gridVAO, size_t vertexCount);
 
+class Object {
+    public:
+        GLuint VAO, VBO;
+        glm::vec3 position = glm::vec3(400, 300, 0);
+        glm::vec3 velocity = glm::vec3(0, 0, 0);
+        size_t vertexCount;
+        glm::vec4 color = glm::vec4(0.0f, 1.0f, 0.0f, 1.0f);
+
+        bool Initializing = false;
+        bool Launched = false;
+        bool Target = false;
+
+        float mass;
+        float density; // kg / m^3 HYDROGEN
+        float radius;
+
+        glm::vec3 LastPos = position;
+
+        Object(glm::vec3 initPosition, glm::vec3 initVelocity, float mass, float density = 3344) {
+            this->position = initPosition;
+            this->velocity = initVelocity;
+            this->mass = mass;
+            this->density = density;
+            this->radius = pow( ((3 * this->mass/this->density)/(4 * 3.14159265359)), (1.0f/3.0f)) / 100000;
+
+            // gen vertices (centered at origin)
+            std::vector<float> vertices = Draw();
+            vertexCount = vertices.size();
+
+            CreateVBOVAO(VAO, VBO, vertices.data(), vertexCount);
+        }
+
+        std::vector<float> Draw() {
+            std::vector<float> vertices;
+            int stacks = 10;
+            int sectors = 10;
+
+            // generate circumference points using integer steps
+            for (float i = 0.0f; i <= stacks; ++i) {
+                float theta1 = (i / stacks) * glm::pi<float>();
+                float theta2 = (i+1) / stacks * glm::pi<float>();
+                for (float j = 0.0f; j < sectors; ++j) {
+                    float phi1 = j / sectors * 2 * glm::pi<float>();
+                    float phi2 = (j+1) / sectors * 2 * glm::pi<float>();
+                    glm::vec3 v1 = sphericalToCartesian(radius, theta1, phi1);
+                    glm::vec3 v2 = sphericalToCartesian(radius, theta1, phi2);
+                    glm::vec3 v3 = sphericalToCartesian(radius, theta2, phi1);
+                    glm::vec3 v4 = sphericalToCartesian(radius, theta2, phi2);
+
+                    // Triangle 1: v1-v2-v3
+                    vertices.insert(vertices.end(), {v1.x, v1.y, v1.z}); //    /|
+                    vertices.insert(vertices.end(), {v2.x, v2.y, v2.z}); //   / |
+                    vertices.insert(vertices.end(), {v3.x, v3.y, v3.z}); //  /__|
+
+                    // Triangle 2: v1-v4-v3
+                    vertices.insert(vertices.end(), {v2.x, v2.y, v2.z});
+                    vertices.insert(vertices.end(), {v4.x, v4.y, v4.z});
+                    vertices.insert(vertices.end(), {v3.x, v3.y, v3.z});
+                }
+            }
+            return vertices;
+        }
+
+        void UpdatePos() {
+            this->position[0] += this->velocity[0] / 94;
+            this->position[1] += this->velocity[1] / 94;
+            this->position[2] += this->velocity[2] / 94;
+            this->radius = pow(((3 * this->mass / this->density)/(4 * 3.14159265359)), (1.0f/3.0f)) / 100000;
+        }
+
+        void UpdateVertices() {
+            // Generate new vertices with current given radius
+            std::vector<float> vertices = Draw();
+
+            // Update the VBO with new vertex data
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+        }
+        glm::vec3 GetPos() const {
+            return this->position;
+        }
+        void accelerate(float x, float y, float z) {
+            this->velocity[0] += x / 96;
+            this->velocity[1] += y / 96;
+            this->velocity[2] += z / 96;
+        }
+
+        float CheckCollision(const Object& other) {
+            float dx = other.position[0] - this->position[0];
+            float dy = other.position[1] - this->position[1];
+            float dz = other.position[2] - this->position[2];
+            float distance = std::pow(dx*dx + dy*dy + dz*dz, (1.0f/2.0f));
+            if (other.radius + this->radius > distance) {
+                return -0.2f;
+            }
+            return 1.0f;
+        }
+};
+std::vector<Object> objs = {};
